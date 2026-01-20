6906582b3e75848a4ecbc412e47338856798acea
MukeshAbhi <abhimukesh284@gmail.com>
2026-01-20 09:46:37 +0000

    working

--- apps/api-service/src/server.ts
+++ apps/api-service/src/server.ts
@@ -1,30 +1,31 @@
 import express from 'express';
 import { rateLimit } from '@url-shortener/rate-limiter';
-import { env } from '@url-shortener/config';
+import 'dotenv/config';
 import { initRedis } from '@url-shortener/redis';
+import { validateEnv } from './utils/envSchema';
 
 initRedis({
-  url: env.UPSTASH_REDIS_REST_URL,
-  token: env.UPSTASH_REDIS_REST_TOKEN,
+  url: validateEnv().UPSTASH_REDIS_REST_URL,
+  token: validateEnv().UPSTASH_REDIS_REST_TOKEN,
 })
 
 const app = express();
+
+app.use(express.json());
+app.use(async (req, res, next) => {
+  const ip = req.ip;
+  const key = `rate:global:${ip}`;
+  const { allowed } = await rateLimit(key, 10, 10 / 60); // 10 per minute
+  if (!allowed) {
+    return res.status(429).json({ error: "Too many requests" });
+  }
+  next();
+});
 
 app.get('/', (req, res) => {
   res.send('Hello From API-Service!');
 });
 
-app.get('/ip', async (req, res) => {
-  const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-  const ipp = req.ip;
-  console.log("ip:", ip,"ipp:", ipp);
-  const key = `rate:create:${ip}`;
-  const { allowed } = await rateLimit(key, 10, 10 / 60); // 10 per minute
-  if (!allowed) {
-    return res.status(429).json({ error: "Too many requests" });
-  }
-  res.send(req.ip);
-});
 
 app.listen(3000, () => {
   console.log('Example app listening on port 3000!');
---
+++ apps/api-service/src/utils/envSchema.ts
@@ -0,0 +1,17 @@
+import z from 'zod';
+
+const envSchema = z.object({
+  UPSTASH_REDIS_REST_URL: z.string(),
+  UPSTASH_REDIS_REST_TOKEN: z.string(),
+});
+
+const validateEnv = () => {
+  const env = envSchema.safeParse(process.env);
+  if (!env.success) {
+    throw new Error('Invalid environment variables');
+  }
+  return env.data;
+};
+
+export { validateEnv };
+
